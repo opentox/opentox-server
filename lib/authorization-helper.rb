1af7ec09719869f11fc54696b90e1e8c4f91660e
rautenberg <rautenberg@in-silico.ch>
2012-03-30 17:57:28 +0200

    temporary https solution

--- lib/authorization-helper.rb
+++ lib/authorization-helper.rb
@@ -55,7 +55,7 @@ module OpenTox
           end
         elsif !env["session"] && subjectid
           unless authorized?(subjectid)
-            $logger.debug "URI not authorized: clean: " + clean_uri("#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}#{request.env['REQUEST_URI']}").to_s + " full: #{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}#{request.env['REQUEST_URI']} with request: #{request.env['REQUEST_METHOD']}"
+            $logger.debug "URI not authorized: clean: " + clean_uri("#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}#{request.env['REQUEST_URI']}").sub("http://","https://").to_s + " full: #{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}#{request.env['REQUEST_URI']} with request: #{request.env['REQUEST_METHOD']}"
             raise OpenTox::NotAuthorizedError.new "Not authorized"
           end
         else
@@ -67,7 +67,7 @@ module OpenTox
       # @param [String]subjectid
       def authorized?(subjectid)
         request_method = request.env['REQUEST_METHOD']
-        uri = clean_uri("#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}#{request.env['REQUEST_URI']}")
+        uri = clean_uri("#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}#{request.env['REQUEST_URI']}").sub("http://","https://")
         request_method = "GET" if request_method == "POST" &&  uri =~ /\/model\/\d+\/?$/
         return OpenTox::Authorization.authorized?(uri, request_method, subjectid)
       end
