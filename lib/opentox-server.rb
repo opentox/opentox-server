bcf008bc2bf51a75f2b042653a9494bd998d7a9f
Andreas Maunz <andreas@maunz.de>
2012-10-30 16:03:42 +0100

    mv'd 4store.rb to client

--- lib/opentox-server.rb
+++ lib/opentox-server.rb
@@ -2,6 +2,5 @@ require 'opentox-client'
 require 'rack'
 require 'rack/contrib'
 require 'sinatra'
-require File.join(File.dirname(__FILE__),"4store.rb")
 require File.join(File.dirname(__FILE__),"opentox.rb")
 require File.join(File.dirname(__FILE__),"authorization-helper.rb")
